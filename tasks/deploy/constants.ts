eba6744b69ac5ea98f9406aaebcd0622893d8fd4
vladi-coti <vladi@coti.io>
2025-06-12 23:08:19 +0300

    deployment fix

--- tasks/deploy/constants.ts
+++ tasks/deploy/constants.ts
@@ -12,6 +12,7 @@ export const FacetNames = [
 	"PartyBPositionActionsFacet",
 	"PartyBQuoteActionsFacet",
 	"PartyBGroupActionsFacet",
+	"PartyBPositionActionsPrivateFacet",
 	"PrivateQuoteFacet",
 ]
 
